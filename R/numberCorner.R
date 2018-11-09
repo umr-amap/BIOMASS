ac2162ba595b800794819c8f4b0b263bd6e7de3c
Arthur Péré <arthur.pere@free.fr>
2018-11-09 14:27:07 +0100

    add an example and correction of bugs

--- R/numberCorner.R
+++ R/numberCorner.R
@@ -22,19 +22,57 @@
 #'   \item[X]{The coordinates X in UTM}
 #'   \item[Y]{The coordinates Y in UTM}
 #'   \item[corner]{The corner numbers}
-#'   \item[codeUTM]{The UTM proj4 code}
 #' }
 #' @export
 #' @importFrom data.table data.table :=
 #' @author Arthur PERE, Maxime REJOU-MECHAIN
 #' @examples
+#' coord <- data.frame(X = c(0, 200, 0, 200), Y = c(0, 0, 200, 200)) + 5000
+#' plot <- rep("plot1", 4)
+#' 
+#' # if you turn clock wise
+#' corner = numberCorner(UTMcoord = coord, plot = plot, origin = c(F, F, T, F), clockWise = T)
+#' 
+#' # Plot the plot
+#' plot(coord, asp = 1)
+#' text(coord, labels = corner$corner, pos = 1)
+#' 
+#' 
+#' # if you turn anti clock wise
+#' corner = numberCorner(UTMcoord = coord, plot = plot, origin = c(F, F, T, F), clockWise = F)
+#' 
+#' # Plot the plot
+#' plot(coord, asp = 1)
+#' text(coord, labels = corner$corner, pos = 1)
 numberCorner <- function(longlat = NULL, UTMcoord = NULL, plot, origin, clockWise) {
+
+
+  # Parameters verification -------------------------------------------------
+
   if (is.null(longlat) && is.null(UTMcoord)) {
     stop("Give me at least one system of coordinate")
   }
   if (!is.null(longlat) && !is.null(UTMcoord)) {
     stop("Please choose between the two coordinate")
   }
+  if (!is.null(longlat)) {
+    if (nrow(longlat) != length(plot)) {
+      stop("Your vector plot isn't the same length of longlat")
+    }
+    if (nrow(longlat) != length(origin)) {
+      stop("Your vector origin isn't the same length of longlat")
+    }
+  }
+  if (!is.null(UTMcoord)) {
+    if (nrow(UTMcoord) != length(plot)) {
+      stop("Your vector plot isn't the same length of UTMcoord")
+    }
+    if (nrow(UTMcoord) != length(origin)) {
+      stop("Your vector origin isn't the same length of UTMcoord")
+    }
+  }
+
+  # data table --------------------------------------------------------------
 
   if (!is.null(longlat)) {
     Coord <- data.table(longlat, Plot = plot)
@@ -52,18 +90,30 @@ numberCorner <- function(longlat = NULL, UTMcoord = NULL, plot, origin, clockWis
 
   Coord[, origin := origin]
 
+  # Function ----------------------------------------------------------------
+
+
 
   # Function to assign corner numbers
   cornerFun <- function(x, y, Origin, clockWise) {
     coord <- data.table(X = x, Y = y, n.row = 1:length(x), corner = as.numeric(NA))
-
+    
+    # if the plot the square is turn at 45° in relation to the horizontal
+    # rotate the coordinate by 45°
+    if(any(rank(coord$X) == 4)){
+      rot = matrix(c(cos(pi/4), sin(pi/4), -sin(pi/4), cos(pi/4)), nrow = 2)
+      newcoord = as.matrix( coord[, .(X, Y)] ) %*% rot
+      
+      coord[, ":="(X = newcoord[,1], Y = newcoord[,2]) ]
+    }
+    
     # Assign temporary corner numbers without accounting for the origin
     m1 <- coord[ rank(X) <= 2, ]
     tmp1 <- m1[rank(Y), n.row]
     m2 <- coord[ rank(X) > 2, ]
     tmp2 <- m2[rank(Y), n.row]
 
-    if (clockWise) {
+    if (!clockWise) {
       coord[tmp1, corner := c(1, 4)]
       coord[tmp2, corner := c(2, 3)]
     } else {
@@ -82,5 +132,5 @@ numberCorner <- function(longlat = NULL, UTMcoord = NULL, plot, origin, clockWis
 
   Coord[, corner := cornerFun(X, Y, origin, clockWise), by = Plot]
 
-  return(as.data.frame(Coord[, .(Plot, X, Y, corner, codeUTM)]))
+  return(as.data.frame(Coord[, .(Plot, X, Y, corner)]))
 }
