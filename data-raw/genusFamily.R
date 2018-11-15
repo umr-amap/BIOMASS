e8c55dd304cc4c5e8c20b458f3e7e4cc8a636292
Arthur Péré <arthur.pere@free.fr>
2018-11-15 15:50:19 +0100

    Data

--- data-raw/genusFamily.R
+++ data-raw/genusFamily.R
@@ -2,173 +2,264 @@ require(rvest)
 require(data.table)
 
 
-# Take all the genus from the genus list of the plant list
-fam = read_html("http://www.theplantlist.org/1.1/browse/-/-/")
-
-# extract the list number 7 where we have all the interesting data
-subnode = fam %>% html_nodes("ul") %>% .[[7]] %>% html_nodes("li") %>% html_nodes("i")
-tplList = data.table( class = subnode %>% html_attr("class"),
-                      fam = subnode %>% html_text)
-rm(fam, subnode)
-
-
-# work on the data, separate the class and family and genus
-tplList = data.table( class = tplList[class != "family", class], 
-                      family = tplList[class == "family", fam], 
-                      genus = tplList[class != "family", fam] )
-
-# remove all the space at the begining of the genus
-tplList[ , genus := gsub('^.+[^A-z]', '', genus)]
+# create from internet the file -------------------------------------------
 
 
 
-source("data-raw/apgfamily.R")
+if (!file.exists('data-raw/genList_tpl.csv')){
+  # Take all the genus from the genus list of the plant list
+  fam = read_html("http://www.theplantlist.org/1.1/browse/-/-/")
+  
+  # extract the list number 7 where we have all the interesting data
+  subnode = fam %>% html_nodes("ul") %>% .[[7]] %>% html_nodes("li") %>% html_nodes("i")
+  tplList = data.table( class = subnode %>% html_attr("class"),
+                        fam = subnode %>% html_text)
+  rm(fam, subnode)
+  
+  
+  # work on the data, separate the class and family and genus
+  tplList = data.table( family = tplList[class == "family", fam], 
+                        genus = tplList[class != "family", fam] )
+  
+  # remove all the space at the begining of the genus
+  tplList[ , genus := gsub('^.[^A-z]', 'x ', genus)]
+  
+  tplList[grepl("^Leguminosae-", family), family := "Fabaceae"]
+  
+  # write the file
+  fwrite(tplList, file = "data-raw/genList_tpl.csv")
+}
+
+tplList = fread("data-raw/genList_tpl.csv")
+
+
+# correct the family ------------------------------------------------------
+
+
+
+# take apg families
+if (!file.exists("apgFamilies.csv")) source("data-raw/apgfamily.R")
 apgFamilies = fread("data-raw/apgFamilies.csv")
-
-
-
-
-
-
-
-
-
-
 
 
 
 # Correct the families (take the apg ones) thanks to apgFamilies (from before)
 tplList = merge(tplList, apgFamilies, by.x = "family", by.y = "famSyn", all.x = T)
-tplList$family[!is.na(tplList$famAPG)] = tplList$famAPG[!is.na(tplList$famAPG)]
+tplList[!is.na(famAPG), family := famAPG]
 
-tplList$order = tplList$famAPG = NULL
+tplList[, ':='(order = NULL, famAPG = NULL, class = NULL)]
 
-#### KEW
 
-opt = 1
 
-if(opt == 1)
-{
-  ## OPTION 1 :  TAKE DIRECTLY FROM KEW
+
+
+
+
+
+
+
+
+
+
+# TO UPDATE KEW GENERA --------------------------------
+
+if (!file.exists("data-raw/vascularKew.csv")){
   
-  #  Take all the genus from kew (see below)
-  kew = read.csv("vascularKew.csv")
-  kew$family[kew$family == "Leguminosae-caesalpinioideae"] = "Fabaceae"
-  kew$family[kew$family == "Leguminosae-mimosoideae"] = "Fabaceae"
-  kew$family[kew$family == "Leguminosae-papilionoideae"] = "Fabaceae"
+  require(purrr)
   
-  # Correct the families (take the apg ones)
-  kew = merge(kew, apgFamilies, by.x = "family", by.y = "famSyn", all.x = T)
-  kew$family[!is.na(kew$famAPG)] = kew$famAPG[!is.na(kew$famAPG)]
-  kew$order = kew$famAPG = NULL
+  listFam = read_html("http://data.kew.org/vpfg1992/genlist.html")
+  listFam = listFam %>% html_nodes("ul") %>% html_nodes("li") %>% html_text
+  listFam = listFam[grepl("[A-Z]{2,}", listFam)]
+  listFam = listFam[!grepl("^FAQ$", listFam)]
+  listFam = gsub(" |\\n|\\t", '', listFam)
+  listFam = sort(listFam)
   
-  kew = unique(kew[!kew$genus %in% tplList$genus, ])
-}
-else
-{
-  ## OPTION 2 : TAKE FROM MAXIME'S DATABASE
+  # And retrieve all the .txt who contains all the genera / family
   
-  # Create a data base with the names absent in genList, to be checked later by TNRS
   
-  kew = unique(read.csv("kewGenera/KewGenera.csv")[, c("familyAPGIII", "accepted")])
-  colnames(kew) = c("family", "genus")
-  kew$genus = gsub("? ", "x ", kew$genus, fixed = T)
-  kew = kew[!is.na(kew$genus), ]
+  file_ = function(x){
+    file = NULL
+    while( !is.data.frame(file) ){
+      file = try( read.csv(paste("http://data.kew.org/vpfg1992/", x, ".TXT", sep = ""), header = F, stringsAsFactors = F) , silent = T )
+      Sys.sleep(0.5)
+    }
+    print(x)
+    colnames(file) = c("num","smthg","genus","Author","numAgain")
+    file$family = paste(substr(x, 1, 1), tolower(substr(x, 2, nchar(x))), sep = "")
+    
+    return(file)
+  }
   
-  # Correct the families (take the apg ones)
-  kew = merge(kew, apgFamilies, by.x = "family", by.y = "famSyn", all.x = T)
-  kew$family[!is.na(kew$famAPG)] = kew$famAPG[!is.na(kew$famAPG)]
-  kew$order = kew$famAPG = NULL
-  kew = kew[!kew$genus %in% tplList$genus, ]
   
-  #### CHECK KEW
-  
-  rm(list = setdiff(ls(), c("apgFamilies", "tplList")))
-  
-  # Keep only the ones that are not in tpl
-  kewTNRS = read.csv("kewGenera/KewSubmitResults.csv")[, c("Name_submitted", "Name_matched", 
-                                                           "Name_matched_accepted_family", "Accepted_name", 
-                                                           "Accepted_name_family", "Selected", "Overall_score")]
-  kewTNRS = kewTNRS[kewTNRS$Selected, ]
-  kewTNRS = unique(kewTNRS[!is.na(kewTNRS$Name_submitted), ])
-  kewTNRS = kewTNRS[kewTNRS$Overall_score >= 0.5, ]
-  
-  # When no accepted name and name matched == name submitted
-  filt = kewTNRS$Accepted_name == "" & kewTNRS$Name_submitted == kewTNRS$Name_matched
-  kewTNRS$Accepted_name[filt] = kewTNRS$Name_submitted[filt]
-  kewTNRS$Accepted_name_family[filt] = kewTNRS$Name_matched_accepted_family[filt]
-  
-  filt = kewTNRS$Accepted_name == ""
-  kewTNRS$Accepted_name[filt] = kewTNRS$Name_matched[filt]
-  
-  kewTNRS = kewTNRS[!kewTNRS$Accepted_name %in% tplList$genus, ]
-  kewTNRS = kewTNRS[!kewTNRS$Name_matched_accepted_family != kewTNRS$Accepted_name_family, ]
-  
-  a = kewTNRS[kewTNRS$genus %in% tplList$genus, ]
-  
-  kew = unique(kewTNRS[, c("Accepted_name", "Accepted_name_family")])
-  colnames(kew) = c("genus", "family")
+  file = map_dfr(listFam, file_)
+  write.csv(file, "data-raw/vascularKew.csv", row.names = F)
 }
 
-#### ADD EVERYTHING
+
+
+
+
+# kew ---------------------------------------------------------------------
+
+kew = fread("data-raw/vascularKew.csv")
+
+# Correct all family with Leguminosae-
+kew[grepl("^Leguminosae-", family), family := "Fabaceae"]
+
+
+# Correct the families (take the apg ones)
+kew = merge(kew, apgFamilies, by.x = "family", by.y = "famSyn", all.x = T)
+kew[!is.na(famAPG), family := famAPG]
+kew[, ':='(order = NULL, famAPG = NULL, num = NULL, smthg = NULL, Author = NULL, numAgain = NULL)]
+
+# take all the genus we don't have in "the plant list"
+kew = kew[!tplList, on = "genus"]
+
+
+
+
+
+
+
+
+
+# Add evrything -----------------------------------------------------------
 
 # Add the remaining Kew genera
 genusFamily = rbind(tplList, kew)
-genusFamily = unique(genusFamily[order(genusFamily$genus), ])
+genusFamily = unique(genusFamily[base::order(genus)])
 
 # Do something for the duplicated genus
-dup = unique(genusFamily$genus[genusFamily$genus %in% genusFamily$genus[duplicated(genusFamily$genus)]])
-# write.csv(dup, "kewGenera/dupGenusToSub.csv", row.names = F)
-if(opt == 1)
-  dupGen = read.csv("dupGenus_TNRS_kew.csv") # result from TNRS of the duplicated genera (those who have more than 1 family)
-else
-  dupGen = read.csv("dupGenus_TNRS.csv")
-dupGen = unique(dupGen[, c("Name_matched", "Name_matched_accepted_family", "Selected")])
-dupGen = dupGen[dupGen$Selected, ] # select valid name from TNRS
+dup = unique( genusFamily[duplicated(genus), genus] )
+write.csv(dup, "data-raw/dupGenusToSub.csv", quote = F, col.names = F)
 
-genusFamily = genusFamily[!genusFamily$genus %in% dupGen$Name_matched, ]
-genusFamily = rbind(genusFamily, data.frame(genus = dupGen$Name_matched, family = dupGen$Name_matched_accepted_family))
-
-genusFamily$family[genusFamily$family %in% "Isoëtaceae"] = "Isoetaceae"
-
-write.csv(genusFamily, file = "genusFamily.csv", row.names = F)
-save(genusFamily, file = "genusFamily.rda", compress = 'xz')
-#
+if(!file.exists("data-raw/dupGenus_TNRS.txt"))
+  stop("Please submite the file 'dupGenusToSub.csv' to this site:\n\t\t",
+       "http://tnrs.iplantcollaborative.org/TNRSapp.html\n", 
+       "\t take the detailed report\n",
+       "\t and deplace/rename it in the folder 'data-raw/dupGenus_TNRS.txt'")
 
 
+dupGen = fread("data-raw/dupGenus_TNRS.txt")
+dupGen = dupGen[Overall_score != 0]
+
+
+dupGen = unique(dupGen[(Selected), .(Name_matched, Name_matched_accepted_family, Selected)])
+genusFamily[dupGen[, .(genus = Name_matched, family = Name_matched_accepted_family)], 
+            on = "genus", family := i.family]
 
 
 
 
-######## TO UPDATE KEW GENERA
-
-# List of all the families in http://data.kew.org/vpfg1992/genfile.html
-listFam = read.csv("famList_kew.csv", header = F)$V1
-
-# And retrieve all the .txt who contains all the genera / family
-fam = listFam[1]
-file = read.csv(paste("http://data.kew.org/vpfg1992/", fam, ".TXT", sep = ""), header = F)
-colnames(file) = c("num","smthg","genus","Author","numAgain")
-file$family = paste(substr(fam, 1, 1), tolower(substr(fam, 2, nchar(fam))), sep = "")
-
-for(fam in listFam[2:length(listFam)])
-{
-  tmp = read.csv(paste("http://data.kew.org/vpfg1992/", fam, ".TXT", sep = ""), header = F)
-  colnames(tmp) = c("num","smthg","genus","Author","numAgain")
-  tmp$family = paste(substr(fam, 1, 1), tolower(substr(fam, 2, nchar(fam))), sep = "")
-  file = rbind(file, tmp)
-}
-file$Genus[file$smthg %in% "X"] = paste("x", file$Genus[file$smthg %in% "X"])
-vascularKew = unique(file[, c("genus", "family")])
-write.csv(vascularKew, file = "kewGenera/vascularKew.csv", row.names = F)
 
 
+# correct few families ------------------------------------------------------
 
-# # Compare it to maxime's kew
-# a = kewTNRS[!kewTNRS$genus %in% vascularKew$genus, ]
-# 
-# kewTNRS$gen = sapply(strsplit(kewTNRS$genus, " ", fixed = T), "[", 1)
-# kewTNRS[grepl(" ", kewTNRS$gen), ]
-# a = kewTNRS[!kewTNRS$gen %in% vascularKew$genus & !kewTNRS$gen %in% tplList$genus, ]
-# 
-# b = vascularKew[!vascularKew$genus %in% kewTNRS$genus, ]
+genusFamily[family == "Isoëtaceae", family := "Isoetaceae"]
+genusFamily[genusFamily$genus=="Acanthocladium","family"]="Compositae"
+genusFamily[genusFamily$genus=="Acanthodium","family"]="Acanthaceae"
+genusFamily[genusFamily$genus=="Acosta","family"]="Compositae"
+genusFamily[genusFamily$genus=="Adelia","family"]="Euphorbiaceae"
+genusFamily[genusFamily$genus=="Alliaria","family"]="Meliaceae"
+genusFamily[genusFamily$genus=="Amblytropis","family"]="Leguminosae"
+genusFamily[genusFamily$genus=="Anacampseros","family"]="Anacampserotaceae"
+genusFamily[genusFamily$genus=="Arthraxon","family"]="Loranthaceae"
+genusFamily[genusFamily$genus=="Asteriscium","family"]="Apiaceae"
+genusFamily[genusFamily$genus=="Balbisia","family"]="Vivianiaceae"
+genusFamily[genusFamily$genus=="Banalia","family"]="Salicaceae"
+genusFamily[genusFamily$genus=="Bartonia","family"]="Gentianaceae"
+genusFamily[genusFamily$genus=="Bassia","family"]="Amaranthaceae"
+genusFamily[genusFamily$genus=="Benthamia","family"]="Boraginaceae"
+genusFamily[genusFamily$genus=="Blechum","family"]="Acanthaceae"
+genusFamily[genusFamily$genus=="Blyttia","family"]="Apocynaceae"
+genusFamily[genusFamily$genus=="Bonapartea","family"]="Asparagaceae"
+genusFamily[genusFamily$genus=="Brachyloma","family"]="Ericaceae"
+genusFamily[genusFamily$genus=="Brachypodium","family"]="Poaceae"
+genusFamily[genusFamily$genus=="Bridgesia","family"]="Sapindaceae"
+genusFamily[genusFamily$genus=="Brocchia","family"]="Compositae"
+genusFamily[genusFamily$genus=="Brodiaea","family"]="Amaryllidaceae"
+genusFamily[genusFamily$genus=="Brugmansia","family"]="Solanaceae"
+genusFamily[genusFamily$genus=="Bruguiera","family"]="Rhizophoraceae"
+genusFamily[genusFamily$genus=="Brya","family"]="Leguminosae"
+genusFamily[genusFamily$genus=="Bulbostylis","family"]="Compositae"
+genusFamily[genusFamily$genus=="Burnettia","family"]="Orchidaceae"
+genusFamily[genusFamily$genus=="Calophyllum","family"]="Calophyllaceae"
+genusFamily[genusFamily$genus=="Candollea","family"]="Dilleniaceae"
+genusFamily[genusFamily$genus=="Cedrus","family"]="Pinaceae"
+genusFamily[genusFamily$genus=="Cladocolea","family"]="Loranthaceae"
+genusFamily[genusFamily$genus=="Coelidium","family"]="Leguminosae"
+genusFamily[genusFamily$genus=="Condalia","family"]="Rhamnaceae"
+genusFamily[genusFamily$genus=="Corynotheca","family"]="Xanthorrhoeaceae"
+genusFamily[genusFamily$genus=="Ctenium","family"]="Poaceae"
+genusFamily[genusFamily$genus=="Cuspidaria","family"]="Bignoniaceae"
+genusFamily[genusFamily$genus=="Cycnia","family"]="Rosaceae"
+genusFamily[genusFamily$genus=="Cynoctonum","family"]="Apocynaceae"
+genusFamily[genusFamily$genus=="Cynodon","family"]="Poaceae"
+genusFamily[genusFamily$genus=="Drummondia","family"]="Orthotrichaceae"
+genusFamily[genusFamily$genus=="Dunalia","family"]="Solanaceae"
+genusFamily[genusFamily$genus=="Dusenia","family"]="Compositae"
+genusFamily[genusFamily$genus=="Duvalia","family"]="Apocynaceae"
+genusFamily[genusFamily$genus=="Esenbeckia","family"]="Rutaceae"
+genusFamily[genusFamily$genus=="Gilibertia","family"]="Meliaceae"
+genusFamily[genusFamily$genus=="Goeppertia","family"]="Lauraceae"
+genusFamily[genusFamily$genus=="Griffithia","family"]="Rubiaceae"
+genusFamily[genusFamily$genus=="Guidonia","family"]="Salicaceae"
+genusFamily[genusFamily$genus=="Haemanthus","family"]="Amaryllidaceae"
+genusFamily[genusFamily$genus=="Harrisonia","family"]="Rutaceae"
+genusFamily[genusFamily$genus=="Harveya","family"]="Orobanchaceae"
+genusFamily[genusFamily$genus=="Hedwigia","family"]="Burseraceae"
+genusFamily[genusFamily$genus=="Heeria","family"]="Anacardiaceae"
+genusFamily[genusFamily$genus=="Holboellia","family"]="Lardizabalaceae"
+genusFamily[genusFamily$genus=="Humboldtia","family"]="Leguminosae"
+genusFamily[genusFamily$genus=="Hygrophila","family"]="Acanthaceae"
+genusFamily[genusFamily$genus=="Isoloma","family"]="Gesneriaceae"
+genusFamily[genusFamily$genus=="Kaulfussia","family"]="Compositae"
+genusFamily[genusFamily$genus=="Lasia","family"]="Araceae"
+genusFamily[genusFamily$genus=="Laxmannia","family"]="Asparagaceae"
+genusFamily[genusFamily$genus=="Leersia","family"]="Poaceae"
+genusFamily[genusFamily$genus=="Lepidostemon","family"]="Convolvulaceae"
+genusFamily[genusFamily$genus=="Limnobium","family"]="Hydrocharitaceae"
+genusFamily[genusFamily$genus=="Malacocarpus","family"]="Zygophyllaceae"
+genusFamily[genusFamily$genus=="Massularia","family"]="Rubiaceae"
+genusFamily[genusFamily$genus=="Microstegium","family"]="Poaceae"
+genusFamily[genusFamily$genus=="Mollia","family"]="Malvaceae"
+genusFamily[genusFamily$genus=="Moniera","family"]="Rutaceae"
+genusFamily[genusFamily$genus=="Myconia","family"]="Compositae"
+genusFamily[genusFamily$genus=="Myrmecophila","family"]="Orchidaceae"
+genusFamily[genusFamily$genus=="Oreocallis","family"]="Ericaceae"
+genusFamily[genusFamily$genus=="Oxymitra","family"]="Annonaceae"
+genusFamily[genusFamily$genus=="Pachyneurum","family"]="Brassicaceae"
+genusFamily[genusFamily$genus=="Pancovia","family"]="Sapindaceae"
+genusFamily[genusFamily$genus=="Parsonsia","family"]="Apocynaceae"
+genusFamily[genusFamily$genus=="Phalangium","family"]="Asparagaceae"
+genusFamily[genusFamily$genus=="Pseudogaltonia","family"]="Asparagaceae"
+genusFamily[genusFamily$genus=="Rauia","family"]="Rutaceae"
+genusFamily[genusFamily$genus=="Rhynchocarpa","family"]="Leguminosae"
+genusFamily[genusFamily$genus=="Richardia","family"]="Araceae"
+genusFamily[genusFamily$genus=="Rostellaria","family"]="Sapotaceae"
+genusFamily[genusFamily$genus=="Solmsia","family"]="Thymelaeaceae"
+genusFamily[genusFamily$genus=="Swartzia","family"]="Leguminosae"
+genusFamily[genusFamily$genus=="Thomandersia","family"]="Thomandersiaceae"
+genusFamily[genusFamily$genus=="Tulasnea","family"]="Melastomataceae"
+genusFamily[genusFamily$genus=="Washingtonia","family"]="Arecaceae"
+genusFamily[genusFamily$genus=="Zappania","family"]="Lamiaceae"
+genusFamily[genusFamily$genus=="Zieria","family"]="Rutaceae"
+
+
+
+
+
+
+
+
+
+# Build the data ----------------------------------------------------------
+
+
+genusFamily = unique(genusFamily)
+fwrite(genusFamily, file = "data-raw/genusFamily.csv")
+usethis::use_data(genusFamily, compress = 'xz')
+
+
+setDT(BIOMASS::genusFamily)[!genusFamily, on = "genus"]
+genusFamily[!setDT(BIOMASS::genusFamily), on = "genus"]
