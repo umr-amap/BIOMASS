eeb423e92c9d6f975f03c5a2af7e6bd6efe9e285
Arthur Péré <arthur.pere@free.fr>
2018-11-15 17:23:12 +0100

    style and completion

--- data-raw/apgfamily.R
+++ data-raw/apgfamily.R
@@ -1,21 +1,21 @@
 require(data.table)
 
 # Construct the APGII families dataset
-apg = fread("data-raw/APGIII.csv", header = F, sep = "\t", quote = "")
-apg = apg[!grepl("^$", V1), ]
+apg <- fread("data-raw/APGIII.csv", header = F, sep = "\t", quote = "")
+apg <- apg[!grepl("^$", V1), ]
 apg[, c("fam1", "fam2", "order") := tstrsplit(V1, "=")]
-apg[, ':='(fam1 = gsub('(^ )|( $)', "", fam1), fam2 = gsub('(^ )|( $)', '', fam2), order = gsub('(^ )|( $)', '', order))]
+apg[, ":="(fam1 = gsub("(^ )|( $)", "", fam1), fam2 = gsub("(^ )|( $)", "", fam2), order = gsub("(^ )|( $)", "", order))]
 
 apg[, V1 := NULL]
 
 # When the order is NA but fam2 isn't : means that fam2 contains the order
-apg[is.na(order), ':='(order = fam2, fam2 = NA_character_)]
+apg[is.na(order), ":="(order = fam2, fam2 = NA_character_)]
 
 
 # Some families have bad character inside, change them
 apg[, fam1 := gsub('"|(\\[)|(\\])|( \\(.+\\))|(\\?)', "", fam1), ]
 apg[, fam2 := gsub('"|(\\[)|(\\])|( \\(.+\\))|(\\?)', "", fam2), ]
-apg[fam1 %in% "Isoëtaceae", fam1 := "Isoetaceae"] 
+apg[fam1 %in% "Isoëtaceae", fam1 := "Isoetaceae"]
 
 
 # Create a new column with the apg families (means, the closest ones from the order on the website)
@@ -26,11 +26,11 @@ apg[is.na(fam2), famAPG := fam1]
 apg[, fam2 := NULL]
 
 # Change the column names
-apgFamilies = unique( apg[base::order(order), .(order, famAPG, famSyn = fam1)] )
+apgFamilies <- unique(apg[base::order(order), .(order, famAPG, famSyn = fam1)])
 
 # Check duplicated families
-a = unique(apgFamilies[, .(order, famAPG)])
-a = a[famAPG %in% famAPG[duplicated(famAPG)], ]
+a <- unique(apgFamilies[, .(order, famAPG)])
+a <- a[famAPG %in% famAPG[duplicated(famAPG)], ]
 
 # And correct the duplicates
 apgFamilies[famAPG == "Equisetaceae", order := "Equisetales"]
@@ -38,11 +38,10 @@ apgFamilies[famAPG == "Orchidaceae", order := "Asparagales"]
 apgFamilies[famAPG == "Sabiaceae", order := "Proteales"]
 
 # Save the file with the old families (to be used to correct the families with the last updated ones)
-apgFamilies = unique(apgFamilies)
-a = apgFamilies
+apgFamilies <- unique(apgFamilies)
 fwrite(apgFamilies, "data-raw/apgFamilies.csv")
 
 apgFamilies[, famSyn := NULL]
-apgFamilies = unique(apgFamilies)
+apgFamilies <- unique(apgFamilies)
 
-usethis::use_data(apgFamilies, compress = 'xz')
+usethis::use_data(apgFamilies, compress = "xz")
--- data-raw/globalwd.R
+++ data-raw/globalwd.R
@@ -1,22 +1,24 @@
 require(devtools)
-require(data.table) 
+require(data.table)
 
-if(!file.exists("data-raw/wdData.csv")){
+if (!file.exists("data-raw/wdData.csv")) {
   require(readxls)
-  
-  url = "http://datadryad.org/bitstream/handle/10255/dryad.235/GlobalWoodDensityDatabase.xls?sequence=1"
-  tmp = tempfile(fileext = ".xls")
+
+  url <- "http://datadryad.org/bitstream/handle/10255/dryad.235/GlobalWoodDensityDatabase.xls?sequence=1"
+  tmp <- tempfile(fileext = ".xls")
   download.file(url, tmp)
 
-  wdData = readxl::read_xls(tmp, sheet = 2)
+  wdData <- readxl::read_xls(tmp, sheet = 2)
   setDT(wdData)
   fwrite(wdData, file = "data-raw/wdData.csv")
 }
 
-wdData = fread("data-raw/wdData.csv", stringsAsFactors = F)
+wdData <- fread("data-raw/wdData.csv", stringsAsFactors = F)
 
-setnames(wdData, c("Wood density (g/cm^3), oven dry mass/fresh volume", "Reference Number", "Family", "Region"), 
-         c("wd", "referenceNumber", "family", "region"))
+setnames(
+  wdData, c("Wood density (g/cm^3), oven dry mass/fresh volume", "Reference Number", "Family", "Region"),
+  c("wd", "referenceNumber", "family", "region")
+)
 
 wdData[, c("genus", "species") := tstrsplit(Binomial, " ", keep = 1:2)]
 
@@ -33,14 +35,15 @@ wdData[region == "South-East_Asia_(tropical)", regionId := "SouthEastAsiaTrop"]
 wdData[region == "Africa_(tropical)", regionId := "AfricaTrop"]
 wdData[region == "South-East_Asia", regionId := "SouthEastAsia"]
 
-wdData = wdData[!is.na(wd),]
+wdData <- wdData[!is.na(wd), ]
 wdData[, Number := seq_along(Number)]
 
-all.equal(wdData[, .(family, genus, species, wd, region, referenceNumber, regionId)], setDT(BIOMASS::wdData), 
-          check.attributes=FALSE)
+all.equal(wdData[, .(family, genus, species, wd, region, referenceNumber, regionId)], setDT(BIOMASS::wdData),
+  check.attributes = FALSE
+)
 
 wdData[ family != BIOMASS::wdData$family, .(Number, family)]
 wdData[, family := getTaxonomy(genus)$family]
 
-diff = wdData[ family != BIOMASS::wdData$family, .(Number, family, genus, species, wd)]
+diff <- wdData[ family != BIOMASS::wdData$family, .(Number, family, genus, species, wd)]
 BIOMASS::wdData[diff$Number, c()]
