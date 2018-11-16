98c86b4ee34d4a0db5807fc88a82a44db1f601d6
Arthur Péré <arthur.pere@free.fr>
2018-11-16 15:13:16 +0100

    data.frame

--- data-raw/KarnatakaForest.R
+++ data-raw/KarnatakaForest.R
@@ -46,4 +46,4 @@ KarnatakaForest <- data_Kar[, .(plotId = PlotID, treeId, family = Family, genus,
 setorder(KarnatakaForest, plotId)
 
 setDF(KarnatakaForest)
-usethis::use_data(KarnatakaForest, overwrite = F)
+usethis::use_data(KarnatakaForest, overwrite = T)
