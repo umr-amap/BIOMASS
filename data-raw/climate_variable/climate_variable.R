22d4b91c9ed7506db733265eb611e84e86d51167
Arthur Péré <arthur.pere@free.fr>
2018-12-13 10:26:57 +0100

    style

--- data-raw/climate_variable/climate_variable.R
+++ data-raw/climate_variable/climate_variable.R
@@ -6,9 +6,11 @@ getData("worldclim", var = "bio", res = 2.5, path = "data-raw/climate_variable")
 
 paste0("data-raw/climate_variable/wc2-5/", c("bio4\\.*", "bio15\\.*"))
 
-files = unlist(sapply(c("bio4", "bio15"), function(x) dir("data-raw/climate_variable/wc2-5", x, full.names = T), 
-              simplify = F), 
-       use.names = F)
+files <- unlist(sapply(c("bio4", "bio15"), function(x) dir("data-raw/climate_variable/wc2-5", x, full.names = T),
+  simplify = F
+),
+use.names = F
+)
 
 zip("data-raw/climate_variable/wc2-5.zip", files = files, flags = "-rj9X")
 
