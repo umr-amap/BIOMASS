cdd4023bdbdc754fee39c80e011f4dea39015de0
Maxime Réjou <maxime.rejou@gmail.com>
2019-02-19 10:07:29 +0100

    Conflict resolved

--- R/summaryByPlot.R
+++ R/summaryByPlot.R
@@ -41,31 +41,31 @@ if (getRversion() >= "2.15.1") {
 #' @importFrom graphics segments
 #' @importFrom stats quantile
 #' @examples
-#' 
+#'
 #' # Load a database
 #' data(NouraguesHD)
 #' data(KarnatakaForest)
-#' 
+#'
 #' # Modelling height-diameter relationship
 #' HDmodel <- modelHD(D = NouraguesHD$D, H = NouraguesHD$H, method = "log2")
-#' 
+#'
 #' # Retrieving wood density values
 #' KarnatakaWD <- getWoodDensity(KarnatakaForest$genus, KarnatakaForest$species,
 #'   stand = KarnatakaForest$plotId
 #' )
-#' 
+#'
 #' # Propagating errors
 #' filt <- KarnatakaForest$plotId %in% c("BSP20", "BSP14")
 #' resultMC <- AGBmonteCarlo(
 #'   D = KarnatakaForest$D[filt], WD = KarnatakaWD$meanWD[filt],
 #'   errWD = KarnatakaWD$sdWD[filt], HDmodel = HDmodel
 #' )
-#' 
+#'
 #' plot <- KarnatakaForest$plotId[ filt ]
-#' 
+#'
 #' # The summary by plot
 #' summaryByPlot(AGB_val = resultMC$AGB_simu, plot)
-#' 
+#'
 #' # The summary by plot for computeAGB
 #' H <- retrieveH(KarnatakaForest$D[filt], model = HDmodel)$H
 #' AGB <- computeAGB(KarnatakaForest$D[filt], WD = KarnatakaWD$meanWD[filt], H = H)
