e8951a6c2bdc81462952092644cb944b3f54658c
Maxime RM <maxime.rejou@gmail.com>
2021-05-20 17:32:02 +0200

    Add  importFrom("methods", "is") to weibull function

--- R/weibullFunction.R
+++ R/weibullFunction.R
@@ -1,6 +1,7 @@
 #' @rdname HDmethods
 #' @return Result of a model (nlsM object)
 #' @importFrom minpack.lm nlsLM nls.lm.control
+#' @importFrom methods is
 
 weibullFunction <- function(data, weight = NULL) {
   ### Compute the weibull model of the H-D relationship
