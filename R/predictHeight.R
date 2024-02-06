1c2816d4f9aeca5f01043d4e5922bdc8a97fe2fd
Guillaume Cornu <gcornu@cirad.fr>
2024-02-06 17:16:42 +0100

    * fix missing export of function predictHeight

--- R/predictHeight.R
+++ R/predictHeight.R
@@ -25,7 +25,7 @@ if (getRversion() >= "2.15.1") {
 #' @author Maxime REJOU-MECHAIN, Ariane TANGUY, Arthur PERE
 #' @seealso [minpack.lm::nlsLM()]
 #'
-#'
+#' @export
 #' @importFrom data.table data.table
 #' @keywords Internal
 predictHeight <- function(D, model, err = FALSE, plot = NULL) {
