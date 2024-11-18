105123c0947cdbc3cf35eda4862f690c4d31a227
ArthurBailly <art.bailly@free.fr>
2024-11-18 18:04:48 +0100

    Improved subplot_summary documentation and vignette associated

--- R/subplot_summary.R
+++ R/subplot_summary.R
@@ -1,109 +1,97 @@
-#' Summarize and display tree information by subplot
+#' Summarise and display tree information by subplot
 #'
 #' @description
+#' After applying the [divide_plot()] function, this function summarises the desired tree metric by sub-plot and displays the plot representation.
 #'
-#' @details
-#' 
-#'
-#' @param subplots output of the function [divide_plot()]
-#' @param value a character indicating the column in subplots$tree_ooord to display
-#' @param fun the function to be applied
+#' @param subplots output of the [divide_plot()] function
+#' @param value a character indicating the column in subplots$tree_df to display
 #' @param draw_plot a logical indicating wether the plot design should be displayed and returned
-#' @param display_trees a logical indicating wether trees should be displayed
+#' @param fun the function to be applied
+#' @param ... optional arguments to fun
 #'
-#' @return a data frame where:
-#'   - `plot`: the code of the plot
-#'
-#' If the `subplot` is set, the output is a list with the previous data frame and a simple features (sf) geometry object.
-#'
+#' @return a list containg the following elements :
+#'  - `tree_summary` : a summary of the metric per subplot 
+#'  - `polygon` : an sf object : simple feature collection of the subplot's polygon
+#'  - `plot_design` : (if draw_plot=T) a ggplot object (or a list of ggplot objects) that can easily be modified
 #'
 #' @export
 #'
-#' @importFrom data.table data.table := first setDT
+#' @importFrom data.table data.table :=
 #' @importFrom grDevices terrain.colors
-#' @importFrom graphics segments
+#' @importFrom ggplot2 ggplot aes geom_sf theme_minimal scale_fill_gradientn theme ggtitle
 #' 
 #' @examples
 #'
 #'
-subplot_summary <- function(subplots, value=NULL, function_name="sum", draw_plot = TRUE, display_trees = FALSE) {
+subplot_summary <- function(subplots, value = NULL, draw_plot = TRUE, fun = sum, ...) {
 
   # Checking parameters --------------------------------------------------------
-  fun <- eval(parse(text = function_name))
   if(is.data.frame(subplots)) {
-    stop("subplots argument does\'nt contain any tree data frame. Use the divide_plot() function with a non-null tree_coord argument")
+    stop("subplots argument does\'nt contain any tree data frame. Use the divide_plot() function with a non-null tree_df argument")
   }
-  if (!is.list(subplots) && any(names(subplots)!=c("sub_corner_coord","tree_coord"))) {
-    stop("subplots argument must be the output of the divide_plot_function(), with a non-null tree_coord argument")
+  if (!is.list(subplots) && any(names(subplots)!=c("sub_corner_coord","tree_df"))) {
+    stop("subplots argument must be the output of the divide_plot_function(), with a non-null tree_df argument")
   }
   if(any(!c("x_proj","y_proj") %in% names(subplots$sub_corner_coord))) {
     subplots$sub_corner_coord[,c("x_proj","y_proj")] <- subplots$sub_corner_coord[,c("x_rel","y_rel")]
-    #stop("projected coordinates are not found in subplots. Use the divide_plot() function with a non-null proj_coord argument (it can be proj_coord = rel_coord if you don\'t have any projected coordinates).")
+    message("projected coordinates are not found in subplots items (no x_proj and y_proj colnames found), tree metric will be summarised in the relative coordinate system")
   }
-  if(!is.null(value) && !value %in% names(subplots$tree_coord)) {
-    stop("value is not a column name of subplots$tree_coord")
+  if(!is.null(value) && !value %in% names(subplots$tree_df)) {
+    stop("value is not a column name of subplots$tree_df")
   }
   if(!is.function(fun)) {
     stop(paste("your function",function_name, "is not a function"))
-  }
-  if(display_trees == T & draw_plot==F) {
-    stop("display_trees cannot be TRUE if draw_plot is FALSE")
-  }
-  if(display_trees == T & any(!c("x_proj","y_proj") %in% names(subplots$tree_coord))) {
-    stop("the tree_coord data-frame in subplots doesn\'t the columns x_proj and y_proj which are required to display the trees. Use the output of divide_plot() function with a non-null proj_coord argument (it can be proj_coord = rel_coord if you don\'t have any projected coordinates).")
+  } else {
+    fun <- match.fun(fun)
   }
 
   # Data processing ------------------------------------------------------------
   corner_dat <- data.table(subplots$sub_corner_coord)
-  tree_summary <- data.table(subplots$tree_coord)[, fun(get(value)) , by=c("subplot_id")]
+  tree_summary <- data.table(subplots$tree_df)[, fun(get(value), ...) , by=c("subplot_id")]
   tree_summary[,plot_id:=sapply(strsplit(tree_summary$subplot_id,"_"),function(x)x[[1]])]#Add plot_id to be able to loop on it
   
-  sf_polygons <- do.call(rbind,lapply(split(corner_dat, by = "subplot_id"), function(data) {
+  sf_polygons <- do.call(rbind,lapply(split(corner_dat, by = "subplot_id"), function(dat) {
     
     # creating polygon
-    mat <- data[, .(x_proj, y_proj)]
+    mat <- dat[, .(x_proj, y_proj)]
     mat <- as.matrix(rbind(mat, mat[1, ]))
     subplot_polygon <- sf::st_polygon(list(mat))
     
-    # value summarized by the function
-    value_fun <- tree_summary[subplot_id == unique(data$subplot_id), V1]
+    # value summarised by the function
+    value_fun <- tree_summary[subplot_id == unique(dat$subplot_id), V1]
     if(identical(value_fun,numeric(0))) value_fun <- 0
     
     # creating data-frame to associate with the polygon
-    df_polygon <- data.frame(unique(data$subplot_id),
+    df_polygon <- data.frame(unique(dat$subplot_id),
                              value_fun,
                              value_fun / sf::st_area(subplot_polygon) * 10000)
     names(df_polygon) <- c("subplot_id",
-                           paste(value, function_name, sep = "_"),
-                           paste(value, function_name, "per_ha", sep = "_"))
+                           paste(value, "summary", sep = "_"),
+                           paste(value, "summary", "per_ha", sep = "_"))
     
-    sf::st_sf(list(subplot_polygon),df_polygon)
+    sf_polygon <- sf::st_sf(list(subplot_polygon),df_polygon)
+    sf::st_geometry(sf_polygon) <- "sf_subplot_polygon"
+    sf_polygon
   }))
+  
+  # Plot the plot(s) -----------------------------------------------------------
   
   if(draw_plot) {
     
     plot_list <- lapply(unique(tree_summary$plot_id), function(plot_id) {
       
-      plot_design <- ggplot() +
-        geom_sf(data = sf_polygons[grep(plot_id,sf_polygons$subplot_id),],
-                mapping = aes(fill=.data[[names(sf_polygons)[3]]])) +
+      plot_design <- ggplot(sf_polygons[grep(plot_id,sf_polygons$subplot_id),]) +
+        geom_sf(mapping = aes(fill=.data[[names(sf_polygons)[3]]])) +
         theme_minimal() + 
         scale_fill_gradientn(colours = rev(terrain.colors(20))) + 
         theme(legend.position="bottom", #legend.title = element_blank(),
               legend.key.size = unit(0.8,"cm"), 
               axis.title = element_blank()
               )
-      if(display_trees) {
-        plot_design <- plot_design + 
-          geom_point(data = subplots$tree_coord[grep(plot_id,subplots$tree_coord$subplot_id),],
-                     mapping = aes(x = x_proj, y = y_proj),
-                     shape = 3, size=0.5)
-      }
       if(plot_id == "subplot") {
-        plot_design <- plot_design + ggtitle(paste(function_name,"of",value,"per ha"))
+        plot_design <- plot_design + ggtitle(paste("Summary of",value,"per ha"))
       } else {
-        plot_design <- plot_design + ggtitle(paste(plot_id,":",function_name,"of",value,"per ha"))
-        
+        plot_design <- plot_design + ggtitle(paste(plot_id,": summary of",value,"per ha"))
       }
         
       print(plot_design)
@@ -112,15 +100,16 @@ subplot_summary <- function(subplots, value=NULL, function_name="sum", draw_plot
     
   }
   
-  setnames(tree_summary, "V1", paste(function_name,value,sep="_"))
+  # Adjusting column names and adding summary per hectare
+  setnames(tree_summary, "V1", paste(value,"summary",sep="_"))
   tree_summary <- data.frame(tree_summary)
-  tree_summary[[paste(function_name,value,"per_ha",sep="_")]] <- sf_polygons[[3]]
+  tree_summary[[paste(value,"summary per_ha",sep="_")]] <- sf_polygons[[3]]
   
-  if(all(tree_summary$plot_id=="subplot")) {
-    tree_summary$plot_id <- NULL
-    plot_list <- plot_list[[1]]
+  if(all(tree_summary$plot_id=="subplot")) { # If just one plot :
+    tree_summary$plot_id <- NULL # delete plot_id column
+    plot_list <- plot_list[[1]] # unlist the output
   } else{
-    tree_summary <- tree_summary[c(3,1,2,4)]
+    tree_summary <- tree_summary[,c(3,1,2,4)]
   }
   
   output <- list(tree_summary = tree_summary, polygon = sf_polygons)
