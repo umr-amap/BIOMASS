f745444108542130969f25afcf1fd8fb9112327f
ArthurBailly <art.bailly@free.fr>
2024-11-29 10:42:50 +0100

    Replaced pkgname.R by BIOMASS-package.R Moved .onLoad and .onAttach codes in zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,18 +1,10 @@
-#' @references
-#' Réjou-Méchain M., Tanguy A., Piponiot C., Chave J., Hérault B. (2017). BIOMASS :
-#' An R Package for estimating above-ground biomass and its uncertainty in tropical forests.
-#' Methods in Ecology and Evolution, 8(9), 1163-1167.
-#'
-#' @keywords internal
-"_PACKAGE"
-
 .onLoad <- function(libname, pkgname) {
   
 }
 
 .onAttach <- function(libname, pkgname) {
   basePath <- cachePath()
-
+  
   if(attr(basePath, "source")=="temp") {
     packageStartupMessage(
       "Using temporary cache",
