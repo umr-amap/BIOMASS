7be6235ada53bc0d8f94ac4a4d7faf63cfcb92c1
ArthurBailly <art.bailly@free.fr>
2025-01-17 12:54:42 +0100

    Deleted deprecated checkPlotCoord()

--- R/globals.R
+++ R/globals.R
@@ -8,9 +8,6 @@ if(getRversion() >= "2.15.1") {
   # check_plot_coord function 
   utils::globalVariables(c("x_proj","y_proj","outlier","Xmean","Ymean","row_number","whatpoint","Xend","Yend"))
   
-  # checkPlotCoord function 
-  utils::globalVariables("nRow")
-  
   # divide_plot function 
   utils::globalVariables(c("x_rel","y_rel","subplot_id","plot_id"))
 
