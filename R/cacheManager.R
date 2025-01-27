afb81408005fcd33ec28dd59074086a26b8a9be9
ArthurBailly <art.bailly@free.fr>
2025-01-27 15:25:28 +0100

    Commented never-used createCache function in cacheManager()

--- R/cacheManager.R
+++ R/cacheManager.R
@@ -14,6 +14,7 @@
 #' @return file path of the resolved cached file.
 #' @importFrom utils download.file unzip
 #' @export 
+#' 
 cacheManager <- function(nameFile) {
   
   if(length(nameFile)>1) {
@@ -107,25 +108,25 @@ cachePath <- function(...) {
   )
 }
 
-#' Function used to create or activate a permanent cache.
-#' 
-#' Permanent cache is located by default in user data dir.
-#' 
-#' You can provide a custom path (that will be defined as a BIOMASS.cache option)
-#' but clearCache function will refuse to operate on it for security reasons.
-#' @param path Use a custom path to host cache
-#' @return No return value, called for side effects
-#' @export
-createCache <- function(path=NULL) {
-  if(is.null(path)) {
-    path <- rappdirs::user_data_dir("R/BIOMASS", NULL)
-  } else {
-    options(BIOMASS.cache=path)
-    message("options(BIOMASS.cache=\"", path, "\")")
-  }
-  dir.create(path, showWarnings = FALSE, recursive = TRUE)
-  invisible(NULL)
-}
+# #' Function used to create or activate a permanent cache.
+# #' 
+# #' Permanent cache is located by default in user data dir.
+# #' 
+# #' You can provide a custom path (that will be defined as a BIOMASS.cache option)
+# #' but clearCache function will refuse to operate on it for security reasons.
+# #' @param path Use a custom path to host cache
+# #' @return No return value, called for side effects
+# #' @export
+# createCache <- function(path=NULL) {
+#   if(is.null(path)) {
+#     path <- rappdirs::user_data_dir("R/BIOMASS", NULL)
+#   } else {
+#     options(BIOMASS.cache=path)
+#     message("options(BIOMASS.cache=\"", path, "\")")
+#   }
+#   dir.create(path, showWarnings = FALSE, recursive = TRUE)
+#   invisible(NULL)
+# }
 
 #' Function to clear cache content and possibly remove it
 #' 
@@ -137,6 +138,7 @@ createCache <- function(path=NULL) {
 #' resulting in deactivating cache as a side effect
 #' @return No return value, called for side effects
 #' @importFrom utils askYesNo
+#' @export
 clearCache <- function(remove=FALSE) {
   basePath <- cachePath()
   
